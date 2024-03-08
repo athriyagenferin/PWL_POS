ce8278059e3b00ac9ff9199dae470f03a00deaa4
Athriya Genferin <2241720075@student.polinema.ac.id>
2024-03-08 20:27:02 +0700

    JS 4 - Practicum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Models\UserModel;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
+        //jobsheet 3
         // coba akses model UserModel
         // $user = UserModel::all(); // ambil semua data dari tabel m_user
         // return view('user', ['data' => $user]);
@@ -24,16 +24,30 @@ class UserController extends Controller
         // ];
 
         // UserModel::insert($data); // tambahkan data ke tabel m_user
+        // //coba akses model UserModel
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
 
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+
+        // UserModel::where('username', 'customer-1')->update($data); // update data ke tabel m_user
+
+        // //coba akses model UserModel
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+
+        //Jobsheet 4
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
+        UserModel::create($data); 
 
-        UserModel::where('username', 'customer-1')->update($data); // update data ke tabel m_user
-
-        //coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        $user = UserModel::all(); 
         return view('user', ['data' => $user]);
-
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,11 +6,26 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UserModel extends Model
+
+//jobsheet 3
+// {
+//     use HasFactory;
+
+//     protected $table = 'm_user'; // Mendefinisikan nama tabel yang digunakan oleh model ini
+//     protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
+
+    
+// }
 {
     use HasFactory;
 
     protected $table = 'm_user'; // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
-
     
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var array
+     */
+    protected $fillable = ['level_id','username','nama',];
 }
