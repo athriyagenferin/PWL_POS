79767550c4ad5d28adaa1220abab71f5928dc56a
Athriya Genferin <2241720075@student.polinema.ac.id>
2024-03-09 21:48:27 +0700

    Practicum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -58,13 +58,20 @@ class UserController extends Controller
         // $user = UserModel::where('level_id', 1)->first();
         // return view('user', ['data' => $user]);
     
-            // $user = UserModel::firstWhere('level_id', 1);
-            // return view('user', ['data' => $user]);
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
 
-        $user = UserModel::findOr(20, ['username', 'nama'],function() {
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username', 'nama'],function() {
+        // abort(404);
+        // });
         
+        // return view('user', ['data' => $user]);
+
+        //Praktikum 2.2 – Not Found Exceptions 
+        // $user = UserModel::findOrFail(1);
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();  
         return view('user', ['data' => $user]);
     }
 }
